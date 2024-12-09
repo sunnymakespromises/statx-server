032fa4885f0d56a77e923e6327a562c17a4d9e80
sunny! <66428984+sunnymakespromises@users.noreply.github.com>
2024-12-08 23:57:54 -0500

    testing types in test route

--- src/routes/test.ts
+++ src/routes/test.ts
@@ -1,17 +1,27 @@
 import fastifyPostgres from "@fastify/postgres"
+import { UUID } from "crypto"
 import { FastifyInstance, FastifyPluginAsync, FastifyReply, FastifyRequest } from "fastify"
 import { PoolClient, QueryResult } from "pg"
+
+type TestQueryResult = {
+    id: UUID,
+    message: string
+}
+
+type TestResponse = {
+    results: TestQueryResult[]
+}
 
 const test: FastifyPluginAsync = async (fastify: FastifyInstance) => {
     fastify.get("/test", async (request: FastifyRequest, reply: FastifyReply) => {
         const client: PoolClient = await fastify.pg.connect()
 
-        const result: QueryResult = await client.query('SELECT * FROM test');
+        const result: QueryResult = await client.query<TestQueryResult>('SELECT * FROM test');
 
         client.release()
 
-        const response = {
-            result: result
+        const response: TestResponse = {
+            results: result.rows
         }
 
         return response
